e6c562399a653772f9ab069c6bc67a11ddda59fd
Aleytia <jarek13705@gmail.com>
2020-01-11 19:03:25 -0800

    Add image instad of mangaURL

--- main.go
+++ main.go
@@ -16,8 +16,12 @@ func rss(w http.ResponseWriter, req *http.Request) {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 
-	feedReg := regexp.MustCompile(`(\t<mangaLink>.+<\/mangaLink>\r\n)`)
-	feed := feedReg.ReplaceAllString(buf.String(), "")
+	//feedReg := regexp.MustCompile(`(\t<mangaLink>.+<\/mangaLink>\r\n)`)
+	//feed := feedReg.ReplaceAllString(buf.String(), "")
+
+	feedReg := regexp.MustCompile(`<mangaLink>\D+(\d+)<\/mangaLink>`)
+	image := `<image><title>Cover</title><link>https://mangadex.cc</link><url>https://mangadex.cc/images/manga/$1.jpg</url></image>`
+	feed := feedReg.ReplaceAllString(buf.String(), image)
 	fmt.Fprintf(w, feed)
 }
 
@@ -27,4 +31,3 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 
 }
-
